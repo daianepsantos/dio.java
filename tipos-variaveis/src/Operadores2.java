18f5f32876c5efeddd4fcdfe69b87f84a8d539fb
Daiane Santos <daianepsantos01@gmail.com>
2023-01-15 00:37:59 -0300

    Update Operadores2.java

--- tipos-variaveis/src/Operadores2.java
+++ tipos-variaveis/src/Operadores2.java
@@ -1,10 +1,11 @@
 public class Operadores2 {
+    
     public static void main(String[] args) {
-        boolean variavel = true;
-        System.out.println(!variavel);
-
-        variavel = !variavel;
-        System.out.println(variavel);
-    }
+        int a, b;
+        a = 6;
+        b = 6;
+        int resultado = a==b ? 1 : 0;
+            System.out.println(resultado);
+        }
 }
 
