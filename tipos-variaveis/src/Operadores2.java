a71ed99f56a291e4296cebb7d05644c892e49c34
Daiane Santos <daianepsantos01@gmail.com>
2023-01-15 01:16:05 -0300

    Criei Operadores3.java

--- tipos-variaveis/src/Operadores2.java
+++ tipos-variaveis/src/Operadores2.java
@@ -1,11 +1,22 @@
 public class Operadores2 {
     
     public static void main(String[] args) {
-        int a, b;
-        a = 6;
-        b = 6;
-        int resultado = a==b ? 1 : 0;
-            System.out.println(resultado);
+        String nomeUm = "DAIANE";
+        String nomeDois = new String("DAIANE");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+        boolean simNao = numero1 > numero2;
+
+        if (numero1 < numero2) {
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numero2?" + simNao);
+
+        System.out.println("numeroUm é maior a numeroDois? " + simNao);
         }
 }
 
