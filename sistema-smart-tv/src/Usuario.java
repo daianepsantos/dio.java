668a6d0a13edfe1a410642da1a0808b9098bdede
Daiane Santos <119690926+daianepsantos@users.noreply.github.com>
2023-01-20 20:35:55 -0300

    Criei o Usuario.java para a SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("TV Ligada:" + smartTv.ligada);
+        System.out.println("Canal atual:" + smartTv.canal);
+        System.out.println("Volume Atual:" + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada:" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada:" + smartTv.ligada);
+    }
+}
