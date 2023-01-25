1be2630390e4c61982029a12afca8b4b7f814b32
Daiane Santos <119690926+daianepsantos@users.noreply.github.com>
2023-01-24 22:28:05 -0300

    Criei um programa para notas de 0 a 10.

--- loops-e-arrays/src/Loops/Ex2_Nota.java
+++ loops-e-arrays/src/Loops/Ex2_Nota.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 //Faça um programa que peça uma nota entre zero e dez.
 
 public class Ex2_Nota {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 || nota > 10) {
+            System.out.println("Nota inválida! Digite novamente. ");
+            nota = scan.nextInt();
+        }
+    }
 }
