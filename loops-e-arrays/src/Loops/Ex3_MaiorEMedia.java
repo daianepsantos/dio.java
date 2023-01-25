f3dc2e3c33dc8cee3aa1192435a1e8d129ac65fa
Daiane Santos <119690926+daianepsantos@users.noreply.github.com>
2023-01-25 20:29:57 -0300

    Update Ex3_MaiorEMedia.java

--- loops-e-arrays/src/Loops/Ex3_MaiorEMedia.java
+++ loops-e-arrays/src/Loops/Ex3_MaiorEMedia.java
@@ -10,6 +10,7 @@ public class Ex3_MaiorEMedia {
 
         int numero;
         int maior = 0;
+        int soma = 0;
 
         int count =0;
 
@@ -17,11 +18,14 @@ public class Ex3_MaiorEMedia {
             System.out.println("Numero: ");
             numero = scan.nextInt(); 
 
+            soma = soma + numero;
+
             if (numero > maior) maior = numero;
 
             count = count +1;
         } while(count < 5);
 
         System.out.println("Maior: " + maior);
+        System.out.println("Media: " + (soma/5));
     }
 }
