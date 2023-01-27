51ddc3e7d8267a8bf3b05575b9da408bc5268242
Daiane Santos <119690926+daianepsantos@users.noreply.github.com>
2023-01-27 18:01:43 -0300

    Conclui o projeto de Números Aleatórios e criei o programa de Arrays Multidimensional

--- loops-e-arrays/src/Arrays/Ex3_NumerosAleatorios.java
+++ loops-e-arrays/src/Arrays/Ex3_NumerosAleatorios.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Random;
+
 /*
  Faça um programa que leia 20 números inteiros aleatórios (entre 0 e 100), armazene-os num vetor.
  Ao final, mostre os números e seus sucessores.
@@ -7,6 +9,28 @@ package Arrays;
 
 public class Ex3_NumerosAleatorios {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int [20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Sucessores dos Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero+1) + " ");
+        }
+
+        System.out.println("Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero-1) + " ");
+        }
     }
 }
---
+++ loops-e-arrays/src/Arrays/Ex4_ArrayMultidimensional.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+import java.util.Random;
+
+public class Ex4_ArrayMultidimensional {
+    public static void main(String[] args) {
+        Random random = new Random();
+    }
+}
