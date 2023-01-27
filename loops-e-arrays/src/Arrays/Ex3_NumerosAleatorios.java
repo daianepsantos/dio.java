7b779f1e01f4ad420df1ce05e9ff93d8ac4933c4
Daiane Santos <119690926+daianepsantos@users.noreply.github.com>
2023-01-27 17:30:37 -0300

    Create Ex3_NumerosAleatorios.java

---
+++ loops-e-arrays/src/Arrays/Ex3_NumerosAleatorios.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+/*
+ Faça um programa que leia 20 números inteiros aleatórios (entre 0 e 100), armazene-os num vetor.
+ Ao final, mostre os números e seus sucessores.
+ */
+
+public class Ex3_NumerosAleatorios {
+    public static void main(String[] args) {
+        
+    }
+}
